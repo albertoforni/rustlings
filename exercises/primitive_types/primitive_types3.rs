ec3cd787a339a157c4cb117ee136049182e48511
Ali Servet Donmez <asd@pittle.org>
2021-02-26 18:08:49 +0100

    Day 3

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,16 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    // https://doc.rust-lang.org/rust-by-example/primitives/array.html
+    let a = [42; 100];
+    // https://stackoverflow.com/a/34684869/11895
+    let a = vec![0; 100];
+    let a = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+
+    // for el in a.iter() {
+    //     println!("{}", el);
+    // }
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");

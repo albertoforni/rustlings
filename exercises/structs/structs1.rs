f87d98efd297b981f3e82a0b288f12024305895d
Alberto Forni <me@albertoforni.com>
2021-03-05 17:29:05 +0100

    2021-03-05

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -1,14 +1,12 @@
 // structs1.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
-struct ColorClassicStruct<'a> {
-    name: &'a str,
-    hex: &'a str,
+struct ColorClassicStruct {
+    name: String,
+    hex: String,
 }
 
-struct ColorTupleStruct<'a>(&'a str, &'a str);
+struct ColorTupleStruct(String, String);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -21,18 +19,21 @@ mod tests {
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
         let green = ColorClassicStruct {
-            name: "green",
-            hex: "#00FF00",
+            name: String::from("green"),
+            hex: String::from("#00FF00"),
         };
 
-        assert_eq!(green.name, "green");
-        assert_eq!(green.hex, "#00FF00");
+        assert_eq!(green.name, String::from("green"));
+        assert_eq!(green.hex, String::from("#00FF00"));
     }
 
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        let green = ColorTupleStruct("green", "#00FF00");
+        let green = ColorTupleStruct(
+            String::from("green"),
+            String::from("#00FF00"),
+        );
 
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
@@ -41,7 +42,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct!
-        // let unit_struct =
+        let unit_struct = UnitStruct;
         let message = format!("{:?}s are fun!", unit_struct);
 
         assert_eq!(message, "UnitStructs are fun!");

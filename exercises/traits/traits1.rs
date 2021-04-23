f54ddcdecaed6bff0ac4b38b7187dce7efccc9e4
Ali Servet Donmez <asd@pittle.org>
2021-04-23 15:43:33 +0200

    Skip bunch of exercises
    … which we already completed together with Alberto

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -7,37 +7,3 @@
 // The trait AppendBar has only one function,
 // which appends "Bar" to any object
 // implementing this trait.
-
-// I AM NOT DONE
-
-trait AppendBar {
-    fn append_bar(self) -> Self;
-}
-
-impl AppendBar for String {
-    //Add your code here
-}
-
-fn main() {
-    let s = String::from("Foo");
-    let s = s.append_bar();
-    println!("s: {}", s);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn is_FooBar() {
-        assert_eq!(String::from("Foo").append_bar(), String::from("FooBar"));
-    }
-
-    #[test]
-    fn is_BarBar() {
-        assert_eq!(
-            String::from("").append_bar().append_bar(),
-            String::from("BarBar")
-        );
-    }
-}

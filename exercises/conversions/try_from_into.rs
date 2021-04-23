567d4c1e0708ed8b27904ab21b6cd3a5cbeb4624
Ali Servet Donmez <asd@pittle.org>
2021-04-23 15:43:08 +0200

    Complete more exercises

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -11,8 +11,6 @@ struct Color {
     blue: u8,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -25,19 +23,44 @@ struct Color {
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = String;
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let range = 0..=255;
+        if range.contains(&tuple.0) && range.contains(&tuple.1) && range.contains(&tuple.2) {
+            return Ok(Color { red: tuple.0 as u8, green: tuple.1 as u8, blue: tuple.2 as u8 });
+        }
+
+        return Err(String::from("Whatever"))
+    }
 }
 
 // Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = String;
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let range = 0..=255;
+        if range.contains(&arr[0]) && range.contains(&arr[1]) && range.contains(&arr[2]) {
+            return Ok(Color { red: arr[0] as u8, green: arr[1] as u8, blue: arr[2] as u8 });
+        }
+
+        return Err(String::from("Whatever"))
+    }
 }
 
 // Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = String;
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(String::from("Slice length must be three"));
+        }
+
+        let range = 0..=255;
+        if range.contains(&slice[0]) && range.contains(&slice[1]) && range.contains(&slice[2]) {
+            return Ok(Color { red: slice[0] as u8, green: slice[1] as u8, blue: slice[2] as u8 });
+        }
+
+        return Err(String::from("Whatever"))
+    }
 }
 
 fn main() {
